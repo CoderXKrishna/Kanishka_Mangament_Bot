5e24e2dd1a68c965ffc27e833840fcecc9d86e5d
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-12-21 06:06:07 -0800

    Update myinfo.py

--- KanishkaBot/modules/myinfo.py
+++ KanishkaBot/modules/myinfo.py
@@ -11,11 +11,11 @@ from KanishkaRobot.events import register
 
 edit_time = 5
 f""" ======================={BOT_NAME}====================== """
-file1 = "https://telegra.ph/file/9a85d0a873e2dd80d278d.jpg"
-file2 = "https://telegra.ph/file/9e7815284031452afa9e5.jpg"
-file3 = "https://telegra.ph/file/dcc5e003287f69acea368.jpg"
+file1 = "https://telegra.ph/file/34dc91ed7b8fb70bc128c.jpg"
+file2 = "https://telegra.ph/file/34dc91ed7b8fb70bc128c.jpg"
+file3 = "https://telegra.ph/file/34dc91ed7b8fb70bc128c.jpg"
 file4 = "https://telegra.ph/file/ed1ce7fee94f46b0f671e.jpg"
-file5 = "https://telegra.ph/file/701028ce085ecfa961a36.jpg"
+file5 = "https://telegra.ph/file/6344973dc3ca79dee5d82.jpg"
 """ ======================={BOT_NAME}====================== """
 
 
